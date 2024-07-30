3a3afcc1ecfe2d7ade6bebbde707ebc1c7cb5dd6
Moth <kampanee.moth@gmail.com>
2024-07-30 10:36:27 +0700

    feat: algorithm skill checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: O(n) เพราะต้องค้นหาข้อมูลจากการเข้าถึงสมาชิกทุกตัวภายใน array
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: O(log n) เพราะค้นหาข้อมูลทีละครึ่ง และลดการค้นหาทีละครึ่งไปเรื่อย ๆ
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: Function findProductPrice มีประสิทธิภาพมากกว่า เพราะถ้ามีข้อมูลมากขึ้นการทำงานของ Function findStudentById จะใช้เวลาในการค้นหานานกว่า Function findProductPrice
 
 
 */
